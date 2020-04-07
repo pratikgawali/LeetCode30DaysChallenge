674f412c0ec2801e0a72999d3658b24f97c2e678
Pratik Gawali <pratik.r.gawali@gmail.com>
2020-04-08 03:25:44 +0530

    Completed Day 7 challenge problem

---
+++ Day7_CountingElements.java
@@ -0,0 +1,113 @@
+/*
+---------------------------------------------------------------------------
+Question
+---------------------------------------------------------------------------
+Given an integer array arr, count element x such that x + 1 is also in arr.
+If there're duplicates in arr, count them seperately.
+ 
+Example 1:
+
+Input: arr = [1,2,3]
+Output: 2
+Explanation: 1 and 2 are counted cause 2 and 3 are in arr.
+
+Example 2:
+
+Input: arr = [1,1,3,3,5,5,7,7]
+Output: 0
+Explanation: No numbers are counted, cause there's no 2, 4, 6, or 8 in arr.
+
+Example 3:
+
+Input: arr = [1,3,2,3,5,0]
+Output: 3
+Explanation: 0, 1 and 2 are counted cause 1, 2 and 3 are in arr.
+
+Example 4:
+
+Input: arr = [1,1,2,2]
+Output: 2
+Explanation: Two 1s are counted cause 2 is in arr.
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+0 <= arr[i] <= 1000
+---------------------------------------------------------------------------
+*/
+package leetcode.challenge;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Day7_CountingElements {
+	
+	private static int countElementsInLessSpace(int[] arr) {
+
+		Arrays.sort(arr);
+       
+        int count = 0, same=1;      
+        for (int i=0; i<arr.length-1; i++) {
+            
+            int diff = arr[i+1] - arr[i];
+            
+            if (diff == 0) {
+                same++;
+            }
+            else {
+                if (diff==1) {
+                    count+=same;
+                }
+                same = 1;
+            }   
+        }
+        
+        return count;
+	}
+	
+	private static int countElementsInRangeSpace(int[] arr) {
+		
+		boolean[] isPresent = new boolean[1002];
+		
+		for (int i=0; i<arr.length; i++) {
+			isPresent[arr[i]] = true;
+		}
+		
+		int count = 0;
+		for (int i=0; i<arr.length; i++) {
+			if (isPresent[arr[i]+1]) {
+				count++;
+			}
+		}
+		
+		return count;
+	}
+
+	private static int countElementsInLessTime(int[] arr) {
+
+		Set<Integer> set = new HashSet<>();
+		for (int i=0; i<arr.length; i++) {
+			set.add(arr[i]);
+		}
+		
+		int count = 0;
+		for (int i=0; i<arr.length; i++) {
+			if (set.contains(arr[i]+1)) {
+				count++;
+			}
+		}
+		
+		return count;
+	}
+	
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 3, 2, 3, 5, 0 };
+		
+		System.out.println("Less space answer: "+ countElementsInLessSpace(arr));
+		System.out.println("Less time answer: "+ countElementsInLessTime(arr));
+		System.out.println("Using counting sort answer: "+ countElementsInRangeSpace(arr));
+	}
+}
